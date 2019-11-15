6193ee4187d2154e94356406bf7d8b7f91aead27
jiminykbob <jikang92@hotmail.com>
2019-11-14 16:30:15 -0800

    add project page and subs

---
+++ src/components/List.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Card, Input } from 'antd';
+import { useModal } from './modals';
+import DeleteListModal from './modals/DeleteListModal';
+import ListTitleForm from './ListTitleForm';
+
+interface Props {
+  id: number;
+  name: string;
+}
+
+const List: React.FC<Props> = ({ id, name }) => {
+  const { showModal } = useModal();
+  const showDeleteListModal = () =>
+    showModal(<DeleteListModal name={name} id={id} />);
+  const handleClick = () => {
+    showDeleteListModal();
+  };
+  return (
+    <Card
+      title={<ListTitleForm title={name} />}
+      style={{ minWidth: 300 }}
+      key={id}
+      extra={<a onClick={handleClick}>...</a>}
+    >
+      <p>task filler</p>
+      <p>task filler</p>
+      <p>task filler</p>
+    </Card>
+  );
+};
+
+export default List;
---
+++ src/components/ListTitleForm.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Form, Input } from 'antd';
+import { FormComponentProps } from 'antd/lib/form';
+import { useUpdateListNameMutation } from '../generated/graphql';
+
+interface Props extends FormComponentProps {
+  title: string;
+}
+
+const ListTitleForm: React.FC<Props> = ({ title, form }) => {
+  const { getFieldDecorator, validateFields } = form;
+  const [updateListName] = useUpdateListNameMutation();
+
+  // submit when fieldtouched && !focus
+  return (
+    <Form>
+      <Form.Item>
+        {getFieldDecorator('name', {
+          rules: [{ required: true, message: 'List name is required' }]
+        })}
+        <Input value={title} />
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Form.create<Props>({ name: 'listTitle' })(ListTitleForm);
