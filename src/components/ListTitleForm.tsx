022efabb2dddeaac79aa8832c6a34b2201832d64
jiminykbob <jikang92@hotmail.com>
2019-11-18 17:18:54 -0800

    add update list name

--- src/components/ListTitleForm.tsx
+++ src/components/ListTitleForm.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Form, Input } from 'antd';
 import { FormComponentProps } from 'antd/lib/form';
 import { useUpdateListNameMutation } from '../generated/graphql';
 
 interface Props extends FormComponentProps {
-  title: string;
+  defaultTitle: string;
+  id: number;
 }
 
-const ListTitleForm: React.FC<Props> = ({ title, form }) => {
-  const { getFieldDecorator, validateFields } = form;
-  const [updateListName] = useUpdateListNameMutation();
+const ListTitleForm: React.FC<Props> = ({ defaultTitle, form, id }) => {
+  const { getFieldDecorator } = form;
+  const inputRef = useRef<Input>(null);
+  const [title, setTitle] = useState(defaultTitle);
+  const [updateListName] = useUpdateListNameMutation({
+    variables: { name: title, id: id.toString() }
+  });
 
-  // submit when fieldtouched && !focus
   return (
     <Form>
       <Form.Item>
         {getFieldDecorator('name', {
           rules: [{ required: true, message: 'List name is required' }]
         })}
-        <Input value={title} />
+        <Input
+          onBlur={() => updateListName()}
+          value={title}
+          ref={inputRef}
+          onChange={e => setTitle(e.target.value)}
+          onPressEnter={() => inputRef.current!.blur()}
+        />
       </Form.Item>
     </Form>
   );
