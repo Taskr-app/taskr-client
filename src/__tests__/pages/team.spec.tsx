98bef64890c7c2b8f9f4d54f0729897378c6e854
Young Chul Lim <youngchullim95@gmail.com>
2019-11-20 18:12:19 -0800

    Add unfinished testing for useUpdateTeamMutation

--- src/__tests__/pages/team.spec.tsx
+++ src/__tests__/pages/team.spec.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { render } from 'enzyme';
+import React, { Component } from 'react';
+import { render, mount } from 'enzyme';
 import TeamPage from '../../pages/team';
 import { MemoryRouter, Route } from "react-router";
 import Hashids from "hashids/cjs";
 import * as HashFactory from "../../lib/hashids";
 import { MockedProvider, wait } from "@apollo/react-testing";
-import { GetUserTeamDocument } from '../../generated/graphql';
+import { GetUserTeamDocument, UpdateTeamDocument } from '../../generated/graphql';
 import { decode } from 'punycode';
 import { act } from 'react-test-renderer';
 
@@ -21,7 +21,7 @@ describe("Pages", () => {
       const mock = {
         request: {
           query: GetUserTeamDocument,
-          variables: {id: "asd"}
+          variables: {id: HashFactory.decode("asd")}
         },
         result: () => {
           getUserTeamQueryCalled = true;
@@ -44,7 +44,7 @@ describe("Pages", () => {
           }
         }
       const wrapper = render(
-        <MockedProvider mocks={[mock]}>
+        <MockedProvider mocks={[mock]} addTypename={false}>
           <MemoryRouter initialEntries={["/team/asd/abc"]}>
             <Route exact path={"/team/:teamId/:teamName"} component={TeamPage}/>
           </MemoryRouter>
@@ -56,6 +56,70 @@ describe("Pages", () => {
       expect(getUserTeamQueryCalled).toBe(true);
     });
 
+    it("should mutate and call useUpdateTeamMutation", async() => {
+      let useUpdateTeamMutationCalled = false;
+      let getUserTeamQueryCalled = false;
+      const mock = {
+        request: {
+          query: UpdateTeamDocument,
+          variables: {
+            teamId: HashFactory.decode("asd"),
+            name: "qwe"
+          }
+        },
+        result: () => {
+          useUpdateTeamMutationCalled = true;
+            return {
+              data: {
+                updateTeam: {
+                  id: "123",
+                  name: "hello"
+                } 
+              }
+            }
+          }
+        }
+
+        const mock2 = {
+          request: {
+            query: GetUserTeamDocument,
+            variables: {id: HashFactory.decode("asd")}
+          },
+          result: () => {
+            getUserTeamQueryCalled = true;
+              return {
+                data: {
+                  getUserTeam: {
+                    id: "123",
+                    name: "hello",
+                    members: {
+                      id: "1",
+                      username: "h"
+                    },
+                    projects: {
+                      id: "2",
+                      name: "p"
+                    }
+                  } 
+                }
+              }
+            }
+          }
+      const wrapper = mount(
+        <MockedProvider mocks={[mock, mock2]} addTypename={false}>
+          <MemoryRouter initialEntries={["/team/asd/abc"]}>
+            <Route exact path={"/team/:teamId/:teamName"} component={TeamPage}/>
+          </MemoryRouter>
+        </MockedProvider>
+      )
+      await act(async() => {
+        await wait(10);
+        wrapper.find('input#teamName').simulate('change', {
+          target: {name: "teamName", value: mock.request.variables.name}
+        });
+      })
+      expect(useUpdateTeamMutationCalled).toBe(true);
+    });
 
   });
 });
