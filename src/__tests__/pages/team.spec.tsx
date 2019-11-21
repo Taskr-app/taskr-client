b6b58b3fa199121367cc5f6d5523adb85973a923
Young Chul Lim <youngchullim95@gmail.com>
2019-11-21 14:44:18 -0800

    Add test for useDeleteTeamProjectMutation

--- src/__tests__/pages/team.spec.tsx
+++ src/__tests__/pages/team.spec.tsx
@@ -5,7 +5,7 @@ import { MemoryRouter, Route } from "react-router";
 import Hashids from "hashids/cjs";
 import * as HashFactory from "../../lib/hashids";
 import { MockedProvider, wait } from "@apollo/react-testing";
-import { GetUserTeamDocument, UpdateTeamDocument } from '../../generated/graphql';
+import { GetUserTeamDocument, UpdateTeamDocument, DeleteTeamProjectDocument } from '../../generated/graphql';
 import { decode } from 'punycode';
 import { act } from "react-dom/test-utils";
 
@@ -27,7 +27,9 @@ describe("Pages", () => {
       const mockQuery = {
         id: HashFactory.decode(routerLocation.teamId),
         name: routerLocation.teamName,
-        newName: "newTeamName"
+        newName: "newTeamName",
+        projectId: "projectName",
+        projectName: "project-01"
       };
 
     it("should render and call getUserTeamQuery", async() => {
@@ -49,8 +51,8 @@ describe("Pages", () => {
                     username: "dev"
                   }],
                   projects: [{
-                    id: 2,
-                    name: "project-01"
+                    id: mockQuery.projectId,
+                    name: mockQuery.projectName
                   }]
                 } 
               }
@@ -102,22 +104,22 @@ describe("Pages", () => {
         const getUserTeamQuery = {
           request: {
             query: GetUserTeamDocument,
-            variables: {id: HashFactory.decode("asd")}
+            variables: {id: mockQuery.id}
           },
           result: () => {
             getUserTeamQueryCalled = true;
               return {
                 data: {
                   getUserTeam: {
-                    id: "123",
-                    name: "hello",
+                    id: mockQuery.id,
+                    name: mockQuery.name,
                     members: [{
-                      id: "1",
-                      username: "h"
+                      id: 1,
+                      username: "dev"
                     }],
                     projects: [{
-                      id: "2",
-                      name: "p"
+                      id: mockQuery.projectId,
+                      name: mockQuery.projectName
                     }]
                   } 
                 }
@@ -150,6 +152,84 @@ describe("Pages", () => {
       expect(getUserTeamQueryCalled).toBe(true);
       expect(useUpdateTeamMutationCalled).toBe(true);
     });
+
+    it("should delete a project and call useDeleteTeamProjectMutation", async() => {
+      let deleteTeamProjectMutationCalled = false;
+      let getUserTeamQueryCalled = false;
+
+      const getUserTeamQuery = {
+        request: {
+          query: GetUserTeamDocument,
+          variables: {id: HashFactory.decode("asd")}
+        },
+        result: () => {
+          getUserTeamQueryCalled = true;
+            return {
+              data: {
+                getUserTeam: {
+                  id: mockQuery.id,
+                  name: mockQuery.name,
+                  members: [{
+                    id: 1,
+                    username: "dev"
+                  }],
+                  projects: [{
+                    id: mockQuery.projectId,
+                    name: mockQuery.projectName
+                  }]
+                } 
+              }
+            }
+          }
+        }
+
+        const deleteTeamProjectMutation = {
+          request: {
+            query: DeleteTeamProjectDocument,
+            variables: {
+              teamId: mockQuery.id,
+              projectId: mockQuery.projectId
+            }
+          },
+          result: () => {
+            deleteTeamProjectMutationCalled = true;
+            return {
+              data: {
+                deleteTeamProject: {
+                  id: mockQuery.projectId,
+                  name: mockQuery.projectName
+                }
+              }
+            }
+          }
+        }
+
+        const wrapper = mount(
+          <MockedProvider mocks={[getUserTeamQuery, deleteTeamProjectMutation, getUserTeamQuery]} addTypename={false}>
+            <MemoryRouter initialEntries={[routerLocation.pathname]}>
+              <Route 
+                exact 
+                path={"/team/:teamId/:teamName"} 
+                render={() => <TeamPage />}
+                // component={TeamPage}
+              />
+            </MemoryRouter>
+          </MockedProvider>
+        )
+        await act(async() => {
+          await wait(0);
+  
+          wrapper.update();
+          wrapper.find(`button#${mockQuery.projectId}`).simulate('click');
+          
+          await wait(0);
+        })
+        expect(getUserTeamQueryCalled).toBe(true);
+        expect(deleteTeamProjectMutationCalled).toBe(true);
+
+    }); 
+
+
 
   });
 });
