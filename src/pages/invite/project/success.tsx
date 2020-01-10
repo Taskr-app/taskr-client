71f4760e773bc201cbc962a56b852da67ac21fce
grantkang5 <g1k05j@gmail.com>
2020-01-10 15:30:05 -0800

    !!Create additional render scenario when user is already authenticated

--- src/pages/invite/project/success.tsx
+++ src/pages/invite/project/success.tsx
@@ -3,13 +3,17 @@ import Layout from '../../../components/layouts/Layout';
 import {
   useMeQuery,
   useAcceptProjectInviteLinkMutation,
-  useValidateLinkQuery
+  useValidateLinkQuery,
+  useLogoutMutation
 } from '../../../generated/graphql';
 import AnonLayout from '../../../components/layouts/AnonLayout';
 import ErrorLayout from '../../../components/layouts/ErrorLayout';
 import { errorMessage } from '../../../lib/messageHandler';
 import { useHistory, useLocation } from 'react-router';
 import { queryStringify, queryParse } from '../../../lib/queryParser';
+import { Redirect } from 'react-router-dom';
+import { Button, message } from 'antd';
+import { setAccessToken } from '../../../lib/accessToken';
 
 const ProjectInviteSuccessPage: React.FC = () => {
   const history = useHistory();
@@ -33,13 +37,28 @@ const ProjectInviteSuccessPage: React.FC = () => {
     onCompleted: () => history.push({ pathname: '/' }),
     onError: err => errorMessage(err)
   });
+  const [logout, { client }] = useLogoutMutation({
+    onCompleted: async () => {
+      setAccessToken('');
+      await client!.clearStore();
+      history.push({
+        pathname: '/login',
+        search: queryStringify({
+          returnUrl: '/invite/project/success',
+          registerKey: 'project-invite',
+          ...routeQueries
+        })
+      });
+    },
+    onError: err => errorMessage(err)
+  })
 
   useEffect(() => {
     if (!routeQueries.id || !routeQueries.email) {
       history.push('/');
     }
 
-    if (data && validated) {
+    if (data && validated && data.me.email === routeQueries.email) {
       const fetchData = async () => {
         await acceptProjectInviteLink();
       };
@@ -76,6 +95,33 @@ const ProjectInviteSuccessPage: React.FC = () => {
   }
   if (!loading && !data) {
     return <AnonLayout handleSignup={handleSignup} handleLogin={handleLogin} />;
+  }
+
+  if (data && data.me.email !== routeQueries.email) {
+
+    const handleAccept = async () => {
+      await logout();
+      message.destroy()
+    }
+
+    message.info(
+      <span style={{ position: 'relative' }}>
+        A project invitation was sent to <b>{routeQueries.email}</b>
+        <br />
+        Do you want to switch accounts?
+        <Button onClick={handleAccept} style={{ margin: '0 5px' }} type='primary'>Log in to a different account</Button>
+        <Button onClick={() => message.destroy()}>Cancel</Button>
+      </span>,
+      0
+    );
+
+    return (
+      <Redirect
+        to={{
+          pathname: '/'
+        }}
+      />
+    );
   }
 
   return (
