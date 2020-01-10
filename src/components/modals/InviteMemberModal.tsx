c0de0b79dc6ddabe391d54d85e59c74a24060f76
grantkang5 <g1k05j@gmail.com>
2020-01-10 15:29:22 -0800

    Extra validations for email filter

--- src/components/modals/InviteMemberModal.tsx
+++ src/components/modals/InviteMemberModal.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Modal, Form, Input, Icon } from 'antd';
+import { Modal, Form, Input, Icon, message } from 'antd';
 import { FormComponentProps } from 'antd/lib/form';
 import { useSendProjectInviteLinkMutation } from '../../generated/graphql';
 import { errorMessage } from '../../lib/messageHandler';
 import { useModal } from '.';
+import { validateEmail } from '../../lib/validateEmail';
 
 interface Props extends FormComponentProps {
   projectId: string;
@@ -13,24 +14,38 @@ const InviteMemberModal: React.FC<Props> = ({ projectId, form }) => {
   const { getFieldDecorator, validateFields } = form;
   const { hideModal } = useModal();
   const unmount = () => hideModal();
-  const [inviteMember] = useSendProjectInviteLinkMutation({
-    onCompleted: () => unmount(),
+  const [inviteMember, { loading }] = useSendProjectInviteLinkMutation({
+    onCompleted: () => {
+      message.success('A project invitation has been successfully sent.');
+      unmount();
+    },
     onError: err => errorMessage(err)
   });
 
   const handleSubmit = () => {
-    validateFields((validationErrors, { email }) => {
-      if (!validationErrors) {
-        inviteMember({ variables: { email, projectId } });
+    validateFields(async (_, { email }) => {
+      const { setFields, getFieldError } = form;
+      if (!validateEmail(email)) {
+        setFields({
+          email: {
+            value: email,
+            errors: [new Error('Not a valid email')]
+          }
+        });
+      }
+      if (!getFieldError('email')) {
+        await inviteMember({ variables: { email, projectId } });
       }
     });
   };
+
   return (
     <Modal
       visible={true}
-      title="Invite Member"
+      title='Invite Member'
       onOk={handleSubmit}
       onCancel={unmount}
+      confirmLoading={loading}
     >
       <Form>
         <Form.Item>
@@ -39,9 +54,9 @@ const InviteMemberModal: React.FC<Props> = ({ projectId, form }) => {
           })(
             <Input
               prefix={
-                <Icon type="align-center" style={{ color: 'rgba(0,0,0,.25' }} />
+                <Icon type='align-center' style={{ color: 'rgba(0,0,0,.25' }} />
               }
-              placeholder="Email"
+              placeholder='Email'
             />
           )}
         </Form.Item>
