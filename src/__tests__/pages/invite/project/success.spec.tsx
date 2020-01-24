5105e1e8e3334690f56a1d131b41e1ba39145d4b
grantkang5 <g1k05j@gmail.com>
2020-01-23 17:18:35 -0800

    add tests for project invites

--- src/__tests__/pages/invite/project/success.spec.tsx
+++ src/__tests__/pages/invite/project/success.spec.tsx
@@ -1,59 +1,45 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
 import { MockedProvider, wait } from '@apollo/react-testing';
-import {
-  ValidateLinkDocument,
-  AcceptProjectInviteLinkDocument,
-} from '../../../../generated/graphql';
+import { AcceptProjectInviteLinkDocument } from '../../../../generated/graphql';
 import ProjectInviteSuccessPage from '../../../../pages/invite/project/success';
 import { act } from 'react-dom/test-utils';
-import { GraphQLError } from 'graphql';
-import ErrorLayout from '../../../../components/layouts/ErrorLayout';
-import AnonLayout from '../../../../components/layouts/AnonLayout';
 import { MemoryRouter, Route, Switch } from 'react-router';
 import Dashboard from '../../../../pages/dashboard';
 import { queryStringify } from '../../../../lib/queryParser';
 import { mockUser } from '../../../utils/mockUser';
 import { mockMeQuery } from '../../../utils/mockQueries';
+import { GraphQLError } from 'graphql';
+import ErrorLayout from '../../../../components/layouts/ErrorLayout';
+import AnonLayout from '../../../../components/layouts/AnonLayout';
+import { AuthenticationError } from '../../../utils/mockErrors';
 
 describe('Pages', () => {
   describe('ProjectInviteSuccessPage', () => {
     const mockQuery = {
       ...mockUser,
       projectInviteLink: 'qwe',
-      key: 'project-invite-dev@email.com',
+      projectId: 1,
+      key: 'project-invite-dev@email.com'
     };
 
     const routerLocation = {
       pathname: '/invite/project/success',
       search: queryStringify({
         email: mockQuery.email,
-        id: mockQuery.projectInviteLink
+        link: mockQuery.projectInviteLink,
+        id: mockQuery.projectId.toString()
       })
     };
 
     let acceptProjectInviteLinkCalled = false;
 
-    const validateLinkQuery = {
-      request: {
-        query: ValidateLinkDocument,
-        variables: {
-          key: mockQuery.key,
-          link: mockQuery.projectInviteLink
-        }
-      },
-      result: {
-        data: {
-          validateLink: true
-        },
-        loading: false
-      }
-    };
-    const acceptProjectLinkQuery = {
+    const acceptProjectLinkMutation = {
       request: {
         query: AcceptProjectInviteLinkDocument,
         variables: {
           email: mockQuery.email,
+          id: mockQuery.projectId.toString(),
           projectInviteLink: mockQuery.projectInviteLink
         }
       },
@@ -61,16 +47,17 @@ describe('Pages', () => {
         acceptProjectInviteLinkCalled = true;
         return {
           data: {
-            acceptProjectInviteLink: mockQuery.projectInviteLink
+            acceptProjectInviteLink: true
           }
         };
-      }
+      },
+      loading: false
     };
 
     it('fires acceptProjectInviteLink mutation on mount', async () => {
       const wrapper = mount(
         <MockedProvider
-          mocks={[mockMeQuery, validateLinkQuery, acceptProjectLinkQuery]}
+          mocks={[mockMeQuery, acceptProjectLinkMutation]}
           addTypename={false}
         >
           <MemoryRouter initialEntries={[routerLocation]}>
@@ -94,17 +81,17 @@ describe('Pages', () => {
       );
 
       await act(async () => {
-        await wait(10);
+        await wait(0);
+        wrapper.update();
       });
 
       expect(acceptProjectInviteLinkCalled).toBe(true);
       expect(routerLocation.pathname).toEqual('/');
-      wrapper.unmount();
     });
 
-    it('should render an error layout if the link has expired', async () => {
+    it('renders <ErrorLayout /> when the mutation results in an error', async () => {
       const errorQuery = {
-        ...validateLinkQuery,
+        ...acceptProjectLinkMutation,
         result: {
           errors: [new GraphQLError('This link has expired')]
         }
@@ -122,13 +109,19 @@ describe('Pages', () => {
         await wait(0);
         wrapper.update();
       });
-
       expect(wrapper.containsMatchingElement(<ErrorLayout />)).toBe(true);
     });
 
-    it('should render an auth form if user is not authenticated', async () => {
+    it('renders <AnonLayout /> when the mutation results in an authentication error', async () => {
+      const authenticationErrorQuery = {
+        ...acceptProjectLinkMutation,
+        result: {
+          errors: [AuthenticationError]
+        }
+      };
+
       const wrapper = mount(
-        <MockedProvider mocks={[validateLinkQuery]} addTypename={false}>
+        <MockedProvider mocks={[authenticationErrorQuery]} addTypename={false}>
           <MemoryRouter initialEntries={[routerLocation]}>
             <ProjectInviteSuccessPage />
           </MemoryRouter>
@@ -137,13 +130,49 @@ describe('Pages', () => {
 
       await act(async () => {
         await wait(0);
+        wrapper.update();
       });
 
+      const handleLogin = (wrapper.instance() as any).handleLogin;
+      const handleSignup = (wrapper.instance() as any).handleSignup;
+
       expect(
-        wrapper.contains(
-          <AnonLayout handleSignup={() => null} handleLogin={() => null} />
+        wrapper.containsMatchingElement(
+          <AnonLayout handleLogin={handleLogin} handleSignup={handleSignup} />
         )
+      ).toBe(true);
+    });
+
+    it('redirects to / and prompts the user if they want to switch accounts if the email query is the same as the user', async () => {
+
+      const wrapper = mount(
+        <MockedProvider mocks={[mockMeQuery, acceptProjectLinkMutation]} addTypename={false}>
+          <MemoryRouter initialEntries={[routerLocation]}>
+            <Switch>
+              <Route
+                exact
+                path='/invite/project/success'
+                render={() => <ProjectInviteSuccessPage />}
+              />
+              <Route
+                exact
+                path='/'
+                render={({ location }) => {
+                  routerLocation.pathname = location.pathname;
+                  return <Dashboard />;
+                }}
+              />
+            </Switch>
+          </MemoryRouter>
+        </MockedProvider>
       );
+
+      await act(async () => {
+        await wait(0);
+        wrapper.update();
+      });
+
+      expect(routerLocation.pathname).toEqual('/');
     });
   });
 });
