fef2755f959cca40f33015429c6ab3fb33315e26
grantkang5 <g1k05j@gmail.com>
2020-01-23 17:19:12 -0800

    add mock utils

---
+++ src/__tests__/utils/mockErrors.tsx
@@ -0,0 +1,13 @@
+export const AuthenticationError = {
+  message: 'Not authenticated',
+  locations: undefined,
+  path: undefined,
+  nodes: undefined,
+  source: undefined,
+  positions: undefined,
+  originalError: undefined,
+  name: 'UNAUTHENTICATED',
+  extensions: {
+    code: 'UNAUTHENTICATED'
+  }
+}
