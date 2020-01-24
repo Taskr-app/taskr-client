9bc3ad351824c43bcc278dbe557c3f138cc8f51f
grantkang5 <g1k05j@gmail.com>
2020-01-23 17:20:00 -0800

    change inline default avatar colors to classes

--- src/components/common/Avatar/index.tsx
+++ src/components/common/Avatar/index.tsx
@@ -1,7 +1,10 @@
 import React, { useCallback, useState } from 'react';
+import cn from 'classnames';
+import styles from './Avatar.module.scss';
 import { User } from '../../../generated/graphql';
 import { Avatar } from 'antd';
 import { cloudinary } from '../../../lib/cloudinary';
+import { AvatarProps } from 'antd/lib/avatar';
 
 export type PartialExceptFor<T, TRequired extends keyof T> = Partial<T> &
   Pick<T, TRequired>;
@@ -11,13 +14,24 @@ interface UserAvatarProps {
   style?: React.CSSProperties;
 }
 
-export const UserAvatar: React.FC<UserAvatarProps> = ({ user, ...style }) => {
-  const [imageError, handleImageError] = useState<boolean>(false)
+export const UserAvatar: React.FC<UserAvatarProps & AvatarProps> = ({
+  user,
+  style,
+  ...avatarProps
+}) => {
+  const [imageError, handleImageError] = useState<boolean>(false);
   const letterToColor = useCallback(() => {
     const colorList = ['#f56a00', '#7265e6', '#ffbf00', '#00a2ae'];
     const letter = user.username[0].toUpperCase();
     return colorList[Math.floor((letter.toUpperCase().charCodeAt(0) - 64) / 6)];
   }, [user.username]);
+
+  const avatarStyle = cn(styles.avatar, {
+    [styles.orange]: letterToColor() === '#f56a00',
+    [styles.purple]: letterToColor() === '#7265e6',
+    [styles.yellow]: letterToColor() === '#ffbf00',
+    [styles.teal]: letterToColor() === '#00a2ae'
+  });
 
   if (user.avatar && !imageError) {
     const imageSource = cloudinary.url(user.avatar, {
@@ -27,17 +41,20 @@ export const UserAvatar: React.FC<UserAvatarProps> = ({ user, ...style }) => {
       gravity: 'face'
     });
 
-    const toggleImageError = () => handleImageError(true)
+    const toggleImageError = () => handleImageError(true);
 
     return (
-      <div>
-        <Avatar icon={<img src={imageSource} alt='' onError={toggleImageError} />} />
-      </div>
+      <Avatar
+        className={avatarStyle}
+        style={{ ...style }}
+        icon={<img src={imageSource} alt='' onError={toggleImageError} />}
+        {...avatarProps}
+      />
     );
   }
 
   return (
-    <Avatar style={{ backgroundColor: letterToColor(), ...style }}>
+    <Avatar className={avatarStyle} style={{ ...style }} {...avatarProps}>
       {user.username[0].toUpperCase()}
     </Avatar>
   );
