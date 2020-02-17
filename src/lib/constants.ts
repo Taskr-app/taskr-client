ea56508cd90fd7360d09266d715bf27c71f430df
jiminykbob <jikang92@hotmail.com>
2020-02-17 13:58:59 -0800

    rewrite taskpos

---
+++ src/lib/constants.ts
@@ -0,0 +1,1 @@
+export const positionBuffer = 16384;
--- src/pages/project/reorder.tsx
+++ src/pages/project/reorder.tsx
@@ -1,17 +1,35 @@
 import { List, Task } from '../../generated/graphql';
 import { DraggableLocation } from 'react-beautiful-dnd';
+import { positionBuffer } from '../../lib/constants';
+
+interface MutationVariables {
+  taskMoved: TaskMovedInput;
+  moreTasks?: TaskInput[];
+}
+
+interface TaskMovedInput extends TaskInput {
+  listId: string;
+}
+
+interface TaskInput {
+  id: string;
+  pos: number;
+}
 
 type ReorderTasksArgs = {
   lists: (Pick<List, 'id' | 'name' | 'pos'> & {
-    tasks: Pick<Task, 'id' | 'name'>[];
+    tasks: Pick<Task, 'id' | 'name' | 'pos'>[];
   })[];
   source: DraggableLocation;
   destination: DraggableLocation;
 };
 
-type ReorderTasksResult = (Pick<List, 'id' | 'name' | 'pos'> & {
-  tasks: Pick<Task, 'id' | 'name'>[];
-})[];
+type ReorderTasksResult = [
+  (Pick<List, 'id' | 'name' | 'pos'> & {
+    tasks: Pick<Task, 'id' | 'name' | 'pos'>[];
+  })[],
+  MutationVariables
+];
 
 const reorder = (lists: any[], startIndex: number, endIndex: number) => {
   const result = Array.from(lists);
@@ -22,48 +40,168 @@ const reorder = (lists: any[], startIndex: number, endIndex: number) => {
 
 export default reorder;
 
-export const reorderTasks = ({
+export const reorderAndAssignTempPos = (
+  lists: any[],
+  startIndex: number,
+  endIndex: number
+) => {
+  const result = Array.from(lists);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  endIndex === 0
+    ? (result[endIndex].pos = 0)
+    : (result[endIndex].pos = result[endIndex - 1].pos + 1);
+  return result;
+};
+
+export const reorderTasks = async ({
   lists,
   source,
   destination
-}: ReorderTasksArgs): ReorderTasksResult => {
-  // (n)
+}: ReorderTasksArgs): Promise<ReorderTasksResult> => {
   const result = lists.map(list => ({
     ...list,
     tasks: Array.from(list.tasks)
   }));
 
-  // moving to same list
+  let sourceList: Pick<List, 'id' | 'name' | 'pos'> & {
+    tasks: Pick<Task, 'id' | 'name' | 'pos'>[];
+  };
+  let destinationList: Pick<List, 'id' | 'name' | 'pos'> & {
+    tasks: Pick<Task, 'id' | 'name' | 'pos'>[];
+  };
+
+  // Find source and destination lists
+  result.forEach(list => {
+    if (source.droppableId.split('-')[1] === list.id) sourceList = list;
+    if (destination.droppableId.split('-')[1] === list.id)
+      destinationList = list;
+  });
+
+  // Moving task
+  const { id, pos } = sourceList!.tasks[source.index];
+  const mutationVariables: MutationVariables = {
+    taskMoved: { id, pos, listId: destinationList!.id }
+  };
+
+  // Task moved in the same list (source)
   if (source.droppableId === destination.droppableId) {
-    // find index of lists where id = droppableId (n)
-    for (let i = 0; i < result.length; i += 1) {
-      if (result[i].id === source.droppableId.split('-')[1]) {
-        result[i].tasks = reorder(
-          result[i].tasks,
-          source.index,
-          destination.index
-        );
-        break;
+    const [
+      repositionedTasks,
+      { pos: newPosition, moreTasks }
+    ] = repositionTasks(
+      destinationList!.tasks,
+      source.index,
+      destination.index
+    );
+    // Assign repositionedtasks to list's tasks
+    destinationList!.tasks = repositionedTasks;
+
+    // Save mutation variables
+    // save updated pos
+    mutationVariables.taskMoved.pos = newPosition;
+    if (moreTasks.length) mutationVariables.moreTasks = moreTasks;
+
+    return [result, mutationVariables];
+  }
+
+  // Task move to another list
+
+  // Removes task in a list and returns the removed task
+  const [removedTask] = sourceList!.tasks.splice(source.index, 1);
+
+  // Append removed task
+  destinationList!.tasks.push(removedTask);
+
+  // Reposition list with new task
+  const [repositionedTasks, { pos: newPosition, moreTasks }] = repositionTasks(
+    destinationList!.tasks,
+    destinationList!.tasks.length - 1,
+    destination.index
+  );
+
+  // Assign repositionedtasks to list's tasks
+  destinationList!.tasks = repositionedTasks;
+
+  // Save mutation variables
+  mutationVariables.taskMoved.pos = newPosition;
+  if (moreTasks.length) mutationVariables.moreTasks = moreTasks;
+
+  return [result, mutationVariables];
+};
+
+const repositionTasks = (
+  list: Pick<Task, 'id' | 'name' | 'pos'>[],
+  startIndex: number,
+  endIndex: number
+): [
+  Pick<Task, 'id' | 'name' | 'pos'>[],
+  { pos: number; moreTasks: TaskInput[] }
+] => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  // Append extra tasks repositioned from collision handling
+  const moreTasksRepositioned: TaskInput[] = [];
+  let newPositionAndMoreTasks: { pos: number; moreTasks: TaskInput[] | [] } = {
+    pos: removed.pos,
+    moreTasks: moreTasksRepositioned
+  };
+
+  // If task is moving to empty list
+  if (result.length === 1) {
+    removed.pos = positionBuffer;
+    newPositionAndMoreTasks.pos = removed.pos;
+  }
+
+  // Assign new position to removed (task)
+  // Move to top of list
+  else if (endIndex === 0) {
+    // Collision handling: reposition every item in the list
+    if (result.length > 1 && result[1].pos <= 1) {
+      let counter = 1;
+      list.forEach(item => {
+        if (item.id === removed.id) {
+          item.pos = positionBuffer;
+        } else {
+          item.pos = positionBuffer * counter + positionBuffer;
+          counter += 1;
+          moreTasksRepositioned.push({ id: item.id, pos: item.pos });
+        }
+      });
+    } else {
+      removed.pos = Math.round(result[1].pos / 2 + Number.EPSILON * 1) / 1;
+      newPositionAndMoreTasks.pos = removed.pos;
+    }
+  }
+  // Move to bottom of list
+  else if (endIndex === result.length - 1) {
+    removed.pos = result[endIndex - 1].pos + positionBuffer;
+    newPositionAndMoreTasks.pos = removed.pos;
+  }
+  // Move in between two items
+  else {
+    // Collision handling: reposition items from endIndex to lastIndex
+    if (Math.abs(result[endIndex - 1].pos - result[endIndex + 1].pos) <= 1) {
+      // Start from endIndex until lastIndex (add task by buffer)
+      result[endIndex].pos = result[endIndex - 1].pos + positionBuffer;
+      // Add following tasks by buffer*2
+      for (let i = endIndex + 1; i < result.length - 1; i += 1) {
+        result[i].pos = result[i - 1].pos + positionBuffer * 2;
+        newPositionAndMoreTasks.pos = result[i].pos;
+        moreTasksRepositioned.push({ id: result[i].id, pos: result[i].pos });
       }
-    }
-    return result;
-  }
-
-  // moving to diff list
-  let task: Pick<Task, 'id' | 'name'>;
-
-  // remove from original (n)
-  for (let i = 0; i < result.length; i += 1) {
-    if (result[i].id === source.droppableId.split('-')[1]) {
-      task = result[i].tasks.splice(source.index, 1)[0];
+    } else {
+      removed.pos =
+        Math.round(
+          ((result[endIndex - 1].pos + result[endIndex + 1].pos) / 2 +
+            Number.EPSILON) *
+            1
+        ) / 1;
+      newPositionAndMoreTasks.pos = removed.pos;
     }
   }
 
-  // insert into another list (n)
-  for (let i = 0; i < result.length; i += 1) {
-    if (result[i].id === destination.droppableId.split('-')[1]) {
-      result[i].tasks.splice(destination.index, 0, task!);
-    }
-  }
-  return result;
+  return [result, newPositionAndMoreTasks];
 };
